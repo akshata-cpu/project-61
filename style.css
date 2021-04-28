0603dcd5cb65a0a077310a5d700c41c6db98a087
akshata-cpu <79588603+akshata-cpu@users.noreply.github.com>
2021-04-28 15:38:02 +0530

    Add files via upload

---
+++ style.css
@@ -0,0 +1,20 @@
+body{
+    background-color: aqua;
+    text-align: center;
+}
+.links {
+    float: left;
+    color: #fff;
+    padding-top: 5px;
+    padding-left: 9px;
+    padding-right: 9px;
+    padding-bottom: 5px;
+    font-family:fantasy;
+    font-size: 20px
+        .my_nav_bar
+        {
+            position: sticky;
+            top: 0;
+            z-index: 99999
+    }
+      
